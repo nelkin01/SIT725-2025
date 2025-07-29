7be440d1f66be594286c4a660680cad6e92e9b77
Nelkin Eldho <65266806+nelkin01@users.noreply.github.com>
2025-07-29 21:16:03 +1000

    Pushing All files for Task 3.2P by Nelkin

---
+++ Task 3.2P/public/js/scripts.js
@@ -0,0 +1,18 @@
+const clickMe = (firstName, lastName) => {
+  alert(`Thanks for logging in, ${firstName} ${lastName}!`);
+}
+
+$(document).ready(function() {
+  $('.materialboxed').materialbox();
+  $('.modal').modal();
+  $('.collapsible').collapsible(); // Initialize collapsible
+
+  // Handle login form submit
+  $('#loginForm').submit(function(e) {
+    e.preventDefault();
+    const firstName = $('#first_name').val();
+    const lastName = $('#last_name').val();
+    $('#loginModal').modal('close');
+    clickMe(firstName, lastName);
+  });
+});
